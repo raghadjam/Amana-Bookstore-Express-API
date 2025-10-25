be36105a9ca915748efe8d18debad1996fe74960
Raghad <raghadjamhour@gmail.com>
2025-10-25 20:25:18 +0200

    first

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
+
 const { authenticate } = require('./utils/auth') // import middleware
 
 
